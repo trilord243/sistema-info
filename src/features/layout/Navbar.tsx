b6dcc5518fe818de4cd014079aa686070fc534ec
CBastidas707 <c.bastidas@correo.unimet.edu.ve>
2024-04-01 11:27:08 -0400

    Spelling mistakes have been corrected

--- src/features/layout/Navbar.tsx
+++ src/features/layout/Navbar.tsx
@@ -243,7 +243,7 @@ const Navbar: React.FC = () => {
                                 "block px-4 py-2 text-sm text-gray-700"
                               )}
                             >
-                              Sign out
+                              Cerrar sesión
                             </a>
                           )}
                         </Menu.Item>
