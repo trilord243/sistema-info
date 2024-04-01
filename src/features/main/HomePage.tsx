1517b944a039e57970bc47e73f45e0e7ebfed7bc
CBastidas707 <c.bastidas@correo.unimet.edu.ve>
2024-04-01 13:29:34 -0400

    news fixed

--- src/features/main/HomePage.tsx
+++ src/features/main/HomePage.tsx
@@ -45,7 +45,7 @@ const HomePage: React.FC = () => {
     "https://firebasestorage.googleapis.com/v0/b/sistema-info-d52b6.appspot.com/o/roversismo.jpg?alt=media&token=acfd3253-89d1-4395-87cc-0467ee0265fb",
     "https://firebasestorage.googleapis.com/v0/b/sistema-info-d52b6.appspot.com/o/Tinoco.jpg?alt=media&token=6c37f924-c840-4362-855f-0c45f2aaa0cc",
     "https://firebasestorage.googleapis.com/v0/b/sistema-info-d52b6.appspot.com/o/conversatorio.jpg?alt=media&token=3accadb8-ab9a-4298-acd2-c362527a99a6",
-    "https://www.unimet.edu.ve/wp-content/uploads/2024/03/FOTOS-CONVERSATORIO-DE-NIXON-8-980x653.jpg",
+    "https://firebasestorage.googleapis.com/v0/b/sistema-info-d52b6.appspot.com/o/progresista.jpg?alt=media&token=38bce9c0-acce-42b2-b9b5-4ab837c3492e",
     "https://www.unimet.edu.ve/wp-content/uploads/2024/03/FOTOS-CONVERSATORIO-DE-NIXON-4-980x653.jpg",
   ];
 
