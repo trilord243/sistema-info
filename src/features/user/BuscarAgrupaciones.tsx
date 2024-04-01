a01eebac2523b67c7c5c2e7575b5eaba3e1aed7a
CBastidas707 <c.bastidas@correo.unimet.edu.ve>
2024-04-01 11:03:48 -0400

    Spelling mistakes have been corrected + filter text have been corrected

--- src/features/user/BuscarAgrupaciones.tsx
+++ src/features/user/BuscarAgrupaciones.tsx
@@ -106,7 +106,8 @@ export const BuscarAgrupaciones = () => {
                 type="nombre"
                 name="nombre"
                 id="nombre"
-                className="block w-full rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full
+                text-white rounded-none rounded-l-md border-0 py-1.5 pl-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="Metrotech"
                 onChange={(e) => setNombreFiltro(e.target.value)}
               />
